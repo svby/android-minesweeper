22d31557581fc77f4d0aa046a9f74c46e3e465b3
stuhlmeier <stuhlmeier@gmail.com>
2018-12-01 09:05:24 +0100

    Modify moves to avoid redundancy
     - unflag flooded cells
     - ignore flagging of revealed cells
    
    Signed-off-by: notiocide <stuhlmeier02@gmail.com>

--- app/src/main/java/at/spengergasse/minesweeper/game/moves/FloodRevealMove.kt
+++ app/src/main/java/at/spengergasse/minesweeper/game/moves/FloodRevealMove.kt
@@ -71,7 +71,10 @@ class FloodRevealMove(val row: Int, val column: Int) : Move {
             if (top in seen || row !in 0 until board.rows || column !in 0 until board.columns) continue
             seen.add(top)
 
-            if (!board.isMine(row, column)) changeSet.reveal(row, column)
+            if (!board.isMine(row, column)) {
+                changeSet.unflag(row, column)
+                changeSet.reveal(row, column)
+            }
 
             if (neighbors(board, row, column).none { board.isMine(it.first, it.second) })
                 neighbors(board, row, column).filter { it !in seen }.forEach(points::push)
