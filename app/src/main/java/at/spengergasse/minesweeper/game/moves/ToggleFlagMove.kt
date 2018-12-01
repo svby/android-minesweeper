a6ce841a58d3a502479b286987491dbf1713adc0
notiocide <stuhlmeier02@gmail.com>
2018-12-01 09:05:24 +0100

    Modify moves to avoid redundancy
     - unflag flooded cells
     - ignore flagging of revealed cells
    
    Signed-off-by: notiocide <stuhlmeier02@gmail.com>

--- app/src/main/java/at/spengergasse/minesweeper/game/moves/ToggleFlagMove.kt
+++ app/src/main/java/at/spengergasse/minesweeper/game/moves/ToggleFlagMove.kt
@@ -5,7 +5,9 @@ import at.spengergasse.minesweeper.game.Board
 class ToggleFlagMove(val row: Int, val column: Int) : Move {
 
     override fun execute(board: Board, changeSet: Board.ChangeSet) {
-        if (board.isFlagged(row, column)) changeSet.unflag(row, column) else changeSet.flag(row, column)
+        if (board.isFlagged(row, column) || board.isRevealed(row, column) || board.isMine(row, column))
+            changeSet.unflag(row, column)
+        else changeSet.flag(row, column)
     }
 
 }
