ff3d48c2274dc52ab807f38e5732febd6f7b062a
notiocide <stuhlmeier02@gmail.com>
2018-12-03 10:48:34 +0100

    Implement remaining flag counter

--- app/src/main/java/at/spengergasse/minesweeper/game/moves/ToggleFlagMove.kt
+++ app/src/main/java/at/spengergasse/minesweeper/game/moves/ToggleFlagMove.kt
@@ -7,7 +7,7 @@ class ToggleFlagMove(val row: Int, val column: Int) : Move {
     override fun execute(board: Board, changeSet: Board.ChangeSet) {
         if (board.isFlagged(row, column) || board.isRevealed(row, column))
             changeSet.unflag(row, column)
-        else changeSet.flag(row, column)
+        else if (board.flagged < board.mines) changeSet.flag(row, column)
     }
 
 }
